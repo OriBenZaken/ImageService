041aeecc20c6d27a30793093849c744949a7af40
Liz <lizaharonian@gmail.com>
2018-04-30 18:58:13 +0300

    fix vmimageservice instalization

--- ImageServiceDesktopApp/VM_ImageService.cs
+++ ImageServiceDesktopApp/VM_ImageService.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace ImageServiceDesktopApp
 {
-    class VM_Settings : IVM_ImageService
+    class VM_ImageService : IVM_ImageService
     {
-        public IImageServiceClient ImageServiceClient { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IImageServiceClient ImageServiceClient { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
