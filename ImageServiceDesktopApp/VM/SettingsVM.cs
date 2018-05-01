363afac9652ef8f926453176ad7cf1e0109444a3
Liz <lizaharonian@gmail.com>
2018-05-01 22:11:30 +0300

    jbhb

--- ImageServiceDesktopApp/VM/SettingsVM.cs
+++ ImageServiceDesktopApp/VM/SettingsVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ImageServiceDesktopApp.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,38 @@ namespace ImageServiceDesktopApp.VM
     class SettingsVM : ISettingsVM
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private ISettingModel model;
+        public SettingsVM(ISettingModel model)
+        {
+            this.model = model;
+            model.PropertyChanged +=
+ delegate (Object sender, PropertyChangedEventArgs e)
+ {
+     NotifyPropertyChanged("VM_" + e.PropertyName);
+     
+ };            vm_handlers = new ObservableCollection<string>();
+        }
+
+        private void NotifyPropertyChanged(string propName)
+        {
+            throw new NotImplementedException();
+        }
+        private ObservableCollection<string> vm_handlers;
+        public ObservableCollection<string> VM_Handlers
+        {
+            get
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    vm_handlers.Add(i.ToString());
+                }
+                return vm_handlers;
+            }
+            set
+            {
+                vm_handlers = value;
+            }
+        }
+
     }
 }
