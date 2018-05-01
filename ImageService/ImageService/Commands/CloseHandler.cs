da918ddbd94ace9facdd94c79d01918035b82647
Liz <lizaharonian@gmail.com>
2018-05-02 00:31:12 +0300

    הוספת תמיכה במחיקת הננדלר בודד

---
+++ ImageService/ImageService/Commands/CloseHandler.cs
@@ -0,0 +1,47 @@
+﻿using ImageService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Commands
+{
+    class CloseHandler : ICommand
+    {
+        public string Execute(string[] args, out bool result)
+        {
+            try
+            {
+                result = true;
+                if (args==null || args.Length == 0)
+                {
+                    throw new Exception("Invalid args for deleting handler");
+                }
+                string toBeDeletedHandler = args[0];
+                string[] directories = (ConfigurationManager.AppSettings.Get("Handler").Split(';'));
+                StringBuilder sbNewHandlers = new StringBuilder();
+                for (int i = 0; i < directories.Length; i++)
+                {
+                    if (directories[i]!=toBeDeletedHandler)
+                    {
+                        sbNewHandlers.Append(directories[i] + ";");
+                    }
+                }
+                string newHandlers = (sbNewHandlers.ToString()).TrimEnd(';');
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                config.AppSettings.Settings["Handler"].Value = newHandlers;
+                config.Save(ConfigurationSaveMode.Modified);
+                //todo: update other customers!!!!!!
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                return ex.ToString();
+            }
+        }
+    }
+}
