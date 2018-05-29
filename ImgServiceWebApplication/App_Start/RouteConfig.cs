c06135299267db628e1e9abb2a88b858de3ed1c2
Liz <lizaharonian@gmail.com>
2018-05-29 22:22:41 +0300

    finish

--- ImgServiceWebApplication/App_Start/RouteConfig.cs
+++ ImgServiceWebApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ImgServiceWebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "First", action = "AjaxView", id = UrlParameter.Optional }
             );
         }
     }
