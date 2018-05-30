5d7dc6d4f06ef4c02b66d91d12e9338bba282883
OriBenZaken <orinetanel1@gmail.com>
2018-05-30 23:02:18 +0300

    Added logs view and controller

---
+++ ImgServiceWebApplication/Controllers/LogsController.cs
@@ -0,0 +1,24 @@
+﻿using ImgServiceWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImgServiceWebApplication.Controllers
+{
+    public class LogsController : Controller
+    {
+        static List<Log> logsEntries = new List<Log>()
+        {
+          new Log  { EntryType = "Info", Message = "Hi" },
+          new Log  { EntryType = "Info", Message = "Bye" }
+        };
+
+        // GET: Logs
+        public ActionResult Logs()
+        {
+            return View(logsEntries);
+        }
+    }
+}
---
+++ ImgServiceWebApplication/Models/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ImgServiceWebApplication.Models
+{
+    public class Log
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Enrty Type")]
+        public string EntryType { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+    }
+}
