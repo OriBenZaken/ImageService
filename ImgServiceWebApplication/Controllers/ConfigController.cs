3abef0e0c68d772bfb7de819bde11359ba2e75e8
Liz <lizaharonian@gmail.com>
2018-05-30 22:01:54 +0300

    config

---
+++ ImgServiceWebApplication/Controllers/ConfigController.cs
@@ -0,0 +1,26 @@
+﻿using ImgServiceWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImgServiceWebApplication.Controllers
+{
+    public class ConfigController : Controller
+    {
+        static Config config = new Config()
+        {
+            SourceName = "mysourcename",
+            LogName = "mylogname",
+            OutputDirectory = "myoutputdirectory",
+            ThumbnailSize = 2,
+            Handlers = new List<string>() { "handler1", "handler2", "handler3" }
+        };
+        // GET: Config
+        public ActionResult Config()
+        {
+            return View(config);
+        }
+    }
+}
---
+++ ImgServiceWebApplication/Models/Config.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ImgServiceWebApplication.Models
+{
+    public class Config
+    {
+
+        [Required]
+        [Display(Name = "Output Directory")]
+        public string OutputDirectory { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Source Name")]
+        public string SourceName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Log Name")]
+        public string LogName { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Thumbnail Size")]
+        public int ThumbnailSize { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Handlers")]
+        public List<string> Handlers { get; set; }
+
+    }
+}
