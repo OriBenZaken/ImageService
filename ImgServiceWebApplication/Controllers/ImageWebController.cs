f75f7559ed9db61204353c79efec21018c5b0ae0
Liz <lizaharonian@gmail.com>
2018-06-01 17:10:45 +0300

    image web fixes

--- ImgServiceWebApplication/Controllers/ImageWebController.cs
+++ ImgServiceWebApplication/Controllers/ImageWebController.cs
@@ -10,6 +10,19 @@ namespace ImgServiceWebApplication.Models
 {
     public class ImageWebController : Controller
     {
+        static ImageWebInfo ImageViewInfo = new ImageWebInfo();
+        public ImageWebController()
+        {
+            ImageViewInfo.NotifyEvent -= Notify;
+            ImageViewInfo.NotifyEvent += Notify;
+
+        }
+        void Notify()
+        {
+            RedirectToAction("ImageWeb");
+
+        }
+
 
         // GET: ImageView
         public ActionResult ImageWeb()
@@ -18,39 +31,6 @@ namespace ImgServiceWebApplication.Models
             ViewBag.IsConnected = ImageViewInfo.IsConnected;
             return View(ImageViewInfo);
         }
-        static ImageWebInfo ImageViewInfo = new ImageWebInfo()
-        {
-            IsConnected = GetServiceStatus(),
-            NumofPics = GetNumOfPics(),
-            Students = GetStudents()
-        };
-       
-
-        public static Boolean GetServiceStatus()
-        {
-            return true;
-        }
-
-        public static int GetNumOfPics()
-        {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\lizah\Desktop\output");
-            int sum = dir.GetFiles().Length;
-            return sum;
-        }
-
-        public static List<Student> GetStudents()
-        {
-            List<Student> students = new List<Student>();
-            StreamReader file = new StreamReader(System.Web.HttpContext.Current.Server.MapPath("~/App_Data/StudentsDetails.txt"));
-            string line;
-
-            while ((line = file.ReadLine()) != null)
-            {
-                string[] param = line.Split(',');
-                students.Add(new Student() { FirstName = param[0], LastName = param[1], ID = param[2] });
-            }
-            file.Close();
-            return students;
-        }
+        
     }
 }
--- ImgServiceWebApplication/Models/ImageWebInfo.cs
+++ ImgServiceWebApplication/Models/ImageWebInfo.cs
@@ -1,13 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
+using static ImgServiceWebApplication.Models.Config;
 
 namespace ImgServiceWebApplication.Models
 {
     public class ImageWebInfo
     {
+        private static Communication.IImageServiceClient GuiClient { get; set; }
+        public event NotifyAboutChange NotifyEvent;
+        private Config m_config;
+
+        public ImageWebInfo()
+        {
+            GuiClient = Communication.ImageServiceClient.Instance;
+            IsConnected = GuiClient.IsConnected;
+            m_config = new Config();
+            m_config.Notify += Notify;
+            NumofPics = 0;
+            Students = GetStudents();
+        }
+        void Notify()
+        {
+            NumofPics = GetNumOfPics(m_config.OutputDirectory);
+            NotifyEvent?.Invoke();
+        }
+
+        public static int GetNumOfPics(string outputDir)
+        {
+            if (outputDir == null || outputDir == "")
+            {
+                return 0;
+            }
+            int counter = 0;
+            while (outputDir == null && (counter < 2)) { System.Threading.Thread.Sleep(1000); counter++; }
+            int sum = 0;
+            DirectoryInfo di = new DirectoryInfo(outputDir);
+            sum += di.GetFiles("*.PNG", SearchOption.AllDirectories).Length;
+            sum += di.GetFiles("*.BMP", SearchOption.AllDirectories).Length;
+            sum += di.GetFiles("*.JPG", SearchOption.AllDirectories).Length;
+            sum += di.GetFiles("*.GIF", SearchOption.AllDirectories).Length;
+            return sum;
+        }
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            StreamReader file = new StreamReader(System.Web.HttpContext.Current.Server.MapPath("~/App_Data/StudentsDetails.txt"));
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] param = line.Split(',');
+                students.Add(new Student() { FirstName = param[0], LastName = param[1], ID = param[2] });
+            }
+            file.Close();
+            return students;
+        }
 
         [Required]
         [Display(Name = "Is Connected")]
